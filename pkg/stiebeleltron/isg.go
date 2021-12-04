c8d450629da0b8fd04ed940c663d1b6b5dfb1d4a
Chris <github.account@chrigel.net>
2021-12-04 14:23:50 +0100

    Implement parser

--- pkg/stiebeleltron/isg.go
+++ pkg/stiebeleltron/isg.go
@@ -2,219 +2,116 @@ package stiebeleltron
 
 import (
 	_ "embed"
+	"fmt"
 	"net/http"
+	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	log "github.com/sirupsen/logrus"
-	"github.com/sp0x/surf"
-	"github.com/sp0x/surf/browser"
 )
 
 type (
-	SystemInfo struct {
-		General struct {
-			// OutsideTemperature in Celsius
-			OutsideTemperature float64
-			// CondenserTemperature in Celsius
-			CondenserTemperature float64
-			// Flow rate in liters/min
-			Flow float64
-			// HeatingCircuitPressure in bar
-			HeatingCircuitPressure float64
-			OutputActivity         struct {
-				// HeatPump activity in %
-				HeatPump float64
-				// WaterPump activity in %
-				WaterPump float64
-			}
-		}
-		RoomTemperature struct {
-			HeatingCircuit1 Temperature
-			HeatingCircuit2 Temperature
-		}
-		DomesticHotWater Temperature
-		Heating          struct {
-			HeatingCircuit1 Temperature
-			HeatingCircuit2 Temperature
-			Flow            struct {
-				// ActualTemperatureHeatPump in Celsius
-				ActualTemperatureHeatPump float64
-				// ActualTemperatureReheating in Celsius
-				ActualTemperatureReheating float64
-				// ActualPreFlowTemperature in Celsius
-				ActualPreFlowTemperature float64
-			}
-			Buffer           Temperature
-			FixedTemperature struct {
-				// SetTemperature in Celsius
-				SetTemperature float64
-			}
-		}
-		ElectricReheating struct {
-			DualModeReheating struct {
-				// DomesticHotWaterTemperature in Celsius
-				DomesticHotWaterTemperature float64
-				// HeatingTemperature in Celsius
-				HeatingTemperature float64
-			}
-		}
-	}
-	HeatPumpInfo struct {
-		Runtime struct {
-			// Heating runtime in hours
-			Heating float64
-			// DomesticHotWater runtime in hours
-			DomesticHotWater float64
-			// Reheating1 runtime in hours
-			Reheating1 float64
-			// Reheating2 runtime in hours
-			Reheating2 float64
-		}
-		// RemainingCompressorRestingTime in seconds
-		RemainingCompressorRestingTime float64
-		// Energy accumulated
-		Energy struct {
-			Heating struct {
-				// Day compressor energy so far
-				Day float64
-				// Total compressor energy so far
-				Total float64
-			}
-			DomesticHotWater struct {
-				// Day compressor energy so far
-				Day float64
-				// Total compressor energy so far
-				Total float64
-			}
-			Reheating struct {
-				// Total compressor energy so far
-				Total float64
-			}
-		}
-	}
-	Temperature struct {
-		// ActualTemperature in Celsius
-		ActualTemperature float64
-		// SetTemperature in Celsius
-		SetTemperature float64
-	}
 	ISGClient struct {
 		Options ClientOptions
-		browser *browser.Browser
+		client  http.Client
 	}
 	ClientOptions struct {
-		URL     string
+		BaseURL string
 		Headers http.Header
 	}
-	Assignments map[string]Property
-	Property    interface {
+	Property interface {
 		GetGroup() string
 		GetSearchString() string
-		GetValue() float64
 		SetValue(v float64)
 	}
-	PropertyImpl struct {
-		Group        string
-		SearchString string
-		value        float64
-	}
+	properties []Property
 	ParseError struct {
-		Group    string
-		Property string
-		Value    string
+		Property Property
+		RawText  string
 		Error    error
 	}
 )
 
 var (
 	PropertyTableQueryExpression = "form#werte table.info tbody"
-
-	//go:embed isg_english.yaml
-	IsgDefinition []byte
+	NumberRegex                  = regexp.MustCompile("([-.,\\d]+)")
 )
 
-func (a *PropertyImpl) GetGroup() string {
-	return a.Group
-}
-
-func (a *PropertyImpl) GetSearchString() string {
-	return a.SearchString
-}
-
-func (a *PropertyImpl) GetValue() float64 {
-	return a.value
-}
-
-func (a *PropertyImpl) SetValue(v float64) {
-	a.value = v
+func (p properties) findProperty(group, searchString string) Property {
+	for _, prop := range p {
+		if prop.GetGroup() == group && prop.GetSearchString() == searchString {
+			return prop
+		}
+	}
+	return nil
 }
 
 // NewISGClient constructs a client for interacting with Stiebel Eltron ISG.
 func NewISGClient(options ClientOptions) (*ISGClient, error) {
-	var br *browser.Browser
-	br = surf.NewBrowser()
-	for key, header := range options.Headers {
-		for _, value := range header {
-			br.AddRequestHeader(key, value)
-		}
-	}
-
 	return &ISGClient{
 		Options: options,
-		browser: br,
+		client:  http.Client{},
 	}, nil
 }
 
-func extractValue(v string) (float64, error) {
-	arr := strings.Split(v, " ")
-	rawValue := strings.ReplaceAll(arr[0], ",", ".")
-	return strconv.ParseFloat(rawValue, 64)
-}
+func (c *ISGClient) ParsePage(urlPath string, properties []Property) ([]ParseError, error) {
 
-func assignValue(a Assignments, group, property string, v float64) {
-	for key, as := range a {
-		if as.GetGroup() == group && as.GetSearchString() == property {
-			as.SetValue(v)
-			log.WithFields(log.Fields{
-				"key":   key,
-				"value": v,
-			}).Debug("Assigned value")
-			return
-		}
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/%s", c.Options.BaseURL, urlPath), nil)
+	if err != nil {
+		return nil, err
 	}
-	log.WithFields(log.Fields{
-		"group":    group,
-		"property": property,
-		"value":    v,
-	}).Warn("Could not find a matching API property")
+	req.Header = c.Options.Headers
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return c.findValues(doc, properties), nil
 }
 
-func (c *ISGClient) ParsePage(callback func(category, key string, value float64)) []ParseError {
+func (c *ISGClient) findValues(doc *goquery.Document, properties properties) []ParseError {
 	var p []ParseError
-	c.browser.Find(PropertyTableQueryExpression).Each(func(i int, selection *goquery.Selection) {
+	doc.Find(PropertyTableQueryExpression).Each(func(i int, selection *goquery.Selection) {
 		group := selection.Find("th").Text()
 		selection.Find("tr.even,tr.odd").Each(func(i int, selection *goquery.Selection) {
 			key := selection.Find("td.key").Text()
-			value := strings.TrimSpace(selection.Find("td.value").Text())
-			parsed, err := extractValue(value)
-			if err != nil {
-				p = append(p, ParseError{
-					Group:    group,
-					Property: key,
-					Value:    value,
-					Error:    err,
-				})
+
+			property := properties.findProperty(group, key)
+			if property == nil {
+				log.WithFields(log.Fields{
+					"group": group,
+					"key":   key,
+				}).Debug("Property not found but not processed")
 				return
 			}
-			log.WithFields(log.Fields{
-				"group": group,
-				"key":   key,
-				"value": parsed,
-			}).Debug("Found property")
-			callback(group, key, parsed)
+
+			cellText := strings.TrimSpace(selection.Find("td.value").Text())
+			parsed, err := c.findNumericValueInCell(cellText)
+			if err != nil {
+				p = append(p, ParseError{
+					Property: property,
+					RawText:  cellText,
+					Error:    err,
+				})
+			}
+			property.SetValue(parsed)
 		})
 	})
 	return p
+}
+
+func(c *ISGClient) findNumericValueInCell(str string) (float64, error) {
+	res := NumberRegex.FindAllStringSubmatch(str, -1)
+	if len(res) == 0 {
+		return 0, fmt.Errorf("could not find a match: " + NumberRegex.String())
+	}
+	match := res[0]
+	replacedComma := strings.ReplaceAll(match[1], ",", ".")
+	return strconv.ParseFloat(replacedComma, 64)
 }
--- pkg/stiebeleltron/isg_test.go
+++ pkg/stiebeleltron/isg_test.go
@@ -1,7 +1,6 @@
 package stiebeleltron
 
 import (
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,84 +9,38 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestISGClient_GetSystemInfo_GivenHTML_WhenDefaultAssignment_ThenParseValues(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		payload, err := ioutil.ReadFile("testdata/systeminfo_1.html")
-		require.NoError(t, err)
-		rw.Write(payload)
-	}))
-	c, err := NewISGClient(ClientOptions{
-		URL: server.URL,
+type stubProperty struct {
+	value        float64
+	group        string
+	searchString string
+}
+
+func (t stubProperty) GetGroup() string {
+	return t.group
+}
+
+func (t stubProperty) GetSearchString() string {
+	return t.searchString
+}
+
+func (t *stubProperty) SetValue(v float64) {
+	t.value = v
+}
+
+func TestISGClient_ParsePage(t *testing.T) {
+	server := httptest.NewServer(http.FileServer(http.Dir("testdata")))
+	defer server.Close()
+
+	client, err := NewISGClient(ClientOptions{
+		BaseURL: server.URL,
+		Headers: nil,
 	})
 	require.NoError(t, err)
-
-	result, parseErrors, err := c.GetSystemInfo(NewSystemInfoDefaultAssignments())
-	assert.NoError(t, err)
-	assert.Empty(t, parseErrors)
-	assert.Equal(t, createExampleSystemInfo(), result)
-}
-
-func TestISGClient_GetHeatPumpInfo_GivenHTML_WhenDefaultAssignment_ThenParseValues(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		payload, err := ioutil.ReadFile("testdata/heatpumpinfo_1.html")
-		require.NoError(t, err)
-		rw.Write(payload)
-	}))
-	c, err := NewISGClient(ClientOptions{
-		URL: server.URL,
-	})
+	prop := &stubProperty{
+		group:        "RUNTIME",
+		searchString: "RNT COMP 1 DHW",
+	}
+	_, err = client.ParsePage("/heatpumpinfo_1.html", []Property{prop})
 	require.NoError(t, err)
-
-	result, parseErrors, err := c.GetHeatPumpInfo(NewHeatPumpInfoDefaultAssignments())
-	assert.NoError(t, err)
-	assert.Empty(t, parseErrors)
-	assert.Equal(t, createExampleHeatPumpInfo(), result)
-}
-
-func createExampleSystemInfo() *SystemInfo {
-	s := &SystemInfo{}
-	s.General.OutsideTemperature = 17.9
-	s.General.OutputActivity.WaterPump = 2.2
-	s.General.OutputActivity.HeatPump = 13
-	s.General.Flow = 0.4
-	s.General.HeatingCircuitPressure = 1.23
-	s.General.CondenserTemperature = 31.4
-
-	s.RoomTemperature.HeatingCircuit1.SetTemperature = 21.6
-	s.RoomTemperature.HeatingCircuit1.ActualTemperature = 23.5
-	s.RoomTemperature.HeatingCircuit2.SetTemperature = 21.6
-	s.RoomTemperature.HeatingCircuit2.ActualTemperature = 23.6
-
-	s.Heating.Flow.ActualTemperatureReheating = 33.9
-	s.Heating.Flow.ActualTemperatureHeatPump = 33.5
-	s.Heating.Flow.ActualPreFlowTemperature = 30.9
-	s.Heating.FixedTemperature.SetTemperature = 42
-	s.Heating.Buffer.SetTemperature = 42
-	s.Heating.Buffer.ActualTemperature = 46.1
-	s.Heating.HeatingCircuit1.ActualTemperature = 46.1
-	s.Heating.HeatingCircuit1.SetTemperature = 42
-	s.Heating.HeatingCircuit2.ActualTemperature = 25.9
-	s.Heating.HeatingCircuit2.SetTemperature = 23.2
-
-	s.ElectricReheating.DualModeReheating.HeatingTemperature = -13
-	s.ElectricReheating.DualModeReheating.DomesticHotWaterTemperature = -13
-
-	s.DomesticHotWater.SetTemperature = 44.5
-	s.DomesticHotWater.ActualTemperature = 47.4
-	return s
-}
-
-func createExampleHeatPumpInfo() *HeatPumpInfo {
-	h := &HeatPumpInfo{}
-	h.RemainingCompressorRestingTime = 1
-	h.Runtime.Reheating1 = 0
-	h.Runtime.Reheating2 = 2
-	h.Runtime.DomesticHotWater = 1771
-	h.Runtime.Heating = 523
-	h.Energy.Heating.Day = 21.145
-	h.Energy.Heating.Total = 56.97
-	h.Energy.DomesticHotWater.Day = 5.052
-	h.Energy.DomesticHotWater.Total = 12.617
-	h.Energy.Reheating.Total = 0.02
-	return h
+	assert.Equal(t, float64(1771), prop.value)
 }
